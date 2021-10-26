82d6b19c71ef9ec6b0cb2f59748a499fb8361fca
Matheus Aurélio <matheusaurelio2004@gmail.com>
2021-10-26 20:37:07 -0300

    Adicionando arquivo de estilo

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sexo e Altura dos Usuários - Arrays</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+    <main>
+        <form action="Calc_Sexo-Altura.php" method="post" class="form">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Entre com o sexo do usuário (User 1):</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="sexoUser1" required>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Entre com a altura do usuário (User 1):</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="alturaUser1" required>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Entre com o sexo do usuário (User 2):</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="sexoUser2" required>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Entre com a altura do usuário (User 2):</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="alturaUser2" required>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Entre com o sexo do usuário (User 3):</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="sexoUser3" required>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Entre com a altura do usuário (User 3):</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="alturaUser3" required>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Entre com o sexo do usuário (User 4):</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="sexoUser4" required>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Entre com a altura do usuário (User 4):</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="alturaUser4" required>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Entre com o sexo do usuário (User 5):</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="sexoUser5" required>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Entre com a altura do usuário (User 5):</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="alturaUser5" required>
+            </div>
+            <fieldset>
+                <button type="submit" class="btn btn-primary">Enviar</button>
+                <button type="reset" class="btn btn-secondary">Limpar</button>
+            </fieldset>
+        </form>
+    </main>
+</body>
+
+</html>
